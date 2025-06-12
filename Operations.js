6f4a1c1153faa73dcf69ec648b25b83fd4b21d3a
vandna-44 <vandnabaman732@gmail.com>
2025-06-12 18:53:04 +0530

    operations

---
+++ Operations.js
@@ -0,0 +1,23 @@
+/*let score = undefined
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+// "33" => convert to  33
+// "33nsh" => // NaN
+// null => //  0
+// true => // 1
+//false => // 0
+*/
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
