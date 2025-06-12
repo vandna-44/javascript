78504f029f37edbeeee2510afd58164a4685e5c4
vandna-44 <vandnabaman732@gmail.com>
2025-06-12 16:21:13 +0530

    datatypes

---
+++ dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+alert(3 + 3) // we are using nodejs ,not browser
+
+let name = "roy"
+let age = 18
+let isLoggedIn = false 
+
+//datatype
+ 
+  //--primitives
+//number => 2 to power 53
+//string => ""
+// Boolean => true/false 
+//null => standalone value / empty
+//undefine => 
+//symbol => unique
+
+
+
+//---non primitives
+//Array
+//object
+
+console.log(typeof age)
