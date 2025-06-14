ec188e64f6bf90b24530c40463ff282a3e63e207
vandna-44 <vandnabaman732@gmail.com>
2025-06-14 22:34:41 +0530

    -

--- Operations.js
+++ Operations.js
@@ -16,8 +16,19 @@ console.log(valueInNumber);
 */
 
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//let isLoggedIn = 1
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=> false 
+//"" => false
+//"hit" => true 
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
+
 
 
