1979f2299f3c4d3b9abda4aaeee780a97a30cfd7
vandna-44 <vandnabaman732@gmail.com>
2025-06-14 23:28:48 +0530

    -

--- Operations.js
+++ Operations.js
@@ -1,3 +1,4 @@
+//-------- convertion -----------------------
 /*let score = undefined
 
 console.log(typeof score);
@@ -27,8 +28,51 @@ console.log(valueInNumber);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber)
+
+
+//---------- Operations---
+let  value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3)
+
+let str1 = "Hello"
+let str2 = "joy"
+
+let str3 = str1+str2
+//console.log(str3)
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2)
+//console.log (1 + 2 + "3")
+
+//console.log ((3+4)*5 % 3);
+// agr string first hai to sabhi string me covert hoga 
+//agr last me hai to phele bale convert ho jayenge 
+
+//console.log(+true);
+//console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
 
 
 
