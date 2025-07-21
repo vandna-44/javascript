dfe4f55ac04f6fed59bfc0f4aa52d1dfce306545
vandna-44 <vandnabaman732@gmail.com>
2025-07-21 17:07:05 +0530

    .

--- strings.js
+++
@@ -1,9 +0,0 @@
-const name = "roy"
-const repoCount = 50
-
-console.log(`Hello my name is ${name} and my repo count is ${ repoCount}`);
-
-const gameName = new String('roy')
-
-console.log(gameName[0]);
-console.log(gameName.__proto__);
