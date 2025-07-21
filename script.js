b68ca19fd76483d9c63cafd0f05f922a4200aaa2
vandna-44 <vandnabaman732@gmail.com>
2025-07-21 17:51:05 +0530

    basic

---
+++ script.js
@@ -0,0 +1,34 @@
+//1.- variables and declarations.
+//variables there are 3 types to use variables to store data in container ->// var,let, const - line-by-line comparison
+//var -> var jo hai (ES5) ka part hai 
+//let and const (ES6) ka part hai
+
+//variable banane ka simple sidha matlb hai ki future me kabhi na kbhi apko eski jarurat pad sakti hai ,or jab bhi eski jarurat padegi you wil all ways have you variables jisko likh ke value ko access kr sakte ho
+
+//variable banane ke tarike 
+var a;
+var a = 12;
+let a;
+let a = 12;
+const a  = 13;
+//const a; ->ese nhi lih sakte kynki yeh error hoga
+
+// declarations and initialization
+ 
+var a; // declare
+var a = 12; // declare and initialize   //initialize ka matlab hota hai first value dena 
+
+//problem to var
+
+// window mein add hota hai
+//function scoped hota hai 
+//aap firse declare kar sakte ho same name se and error nhi ayega 
+// these reson bring to be are not use to let 
+
+
+// to let use krenge yeh apko error dega same name use krne pe jo ki right way hai kyunki same name declare nhi kar sakte
+let a = 12;
+let b = 22;
+
+// const jb use kerte hen jb apko ptaa hoke apko yeh value change nhi karni hai const me aap value change nhi kr sakte ex:pi
+
